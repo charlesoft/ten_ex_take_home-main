9b664e44c9040f380167dc681d409eed725834c4
Charles Furtado <charlees1509@hotmail.com>
2023-07-25 23:42:46 -0300

    Add marvel_api_call schema.

---
+++ lib/ten_ex_take_home/marvel_api_call.ex
@@ -0,0 +1,14 @@
+defmodule TenExTakeHome.MarvelApiCall do
+  @moduledoc false
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  shema "marvel_api_calls" do
+    field :status, :string
+  end
+
+  def changeset(model, attrs \\ %{}) do
+    cast(model, attrs)
+  end
+end
