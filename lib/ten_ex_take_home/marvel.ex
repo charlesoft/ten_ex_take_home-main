a8274b9cae15b870b3fdb6d99d7aabd591cf29a8
Charles Furtado <charlees1509@hotmail.com>
2023-07-25 21:01:18 -0300

    Add default_offset to api request.

--- lib/ten_ex_take_home/marvel.ex
+++ lib/ten_ex_take_home/marvel.ex
@@ -1,12 +1,20 @@
 defmodule TenExTakeHome.Marvel do
-  def get_characters do
+  @default_offset 0
+
+  def get_characters(params \\ %{}) do
+    offset = Map.get(params, :offset)
+    offset = if is_nil(offset), do: @default_offset, else: offset
+
     [host: host, public_api_key: public_api_key, ts: ts, hash: hash] = get_api_credentials()
 
-    case HTTPoison.get("#{host}/v1/public/characters?ts=#{ts}&apikey=#{public_api_key}&hash=#{hash}") do
+    case HTTPoison.get(
+           "#{host}/v1/public/characters?ts=#{ts}&apikey=#{public_api_key}&hash=#{hash}&offset=#{offset}"
+         ) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         %{"data" => %{"results" => results}} = Jason.decode!(body)
 
-          {:ok, results}
+        {:ok, results}
+
       {:error, _error} ->
         {:ok, []}
     end
